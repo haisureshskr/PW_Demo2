48fab11304489277c5438ac90ce5514e06065ac6
haisureshskr <haisureshskr@gmail.com>
2026-09-21 17:09:30 +0530

    first change

--- tests/section03_getting_started/first-test.spec.ts
+++ tests/section03_getting_started/first-test.spec.ts
@@ -4,7 +4,8 @@ test("Load Home Page", async ({ page }) => {
     await page.goto("http://qa-cart.com")
     await expect(page).toHaveTitle(
         "QA TEST AUTOMATION DEMO STORE BY ANURADHA AGARWAL")
-    // await page.locator(".add-to-cart").click();
-    // await page.locator(".product-item").first().click();
-    // await expect(page.locator(".cart-count")).toHaveText("1");
+    await page.locator(".add-to-cart").click();
+    await page.locator(".product-item").first().click();
+    await expect(page.locator(".cart-count")).toHaveText("1");
+
 })
